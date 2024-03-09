4180be76127df548586d7f49542bd738db8c6d46
Cesar Quintini <cesarquintini@gmail.com>
2024-03-09 15:38:31 -0400

    Update sendgrid.js

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,30 +1,23 @@
 #! /usr/bin/env node
 
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const fs = require('fs'),
-    filename = 'hello-world.pdf',
-    fileType = 'application/pdf',
-    data = fs.readFileSync('attachments/' + filename);
-
 const msg = {
-    to: 'peterevans.dev@protonmail.com',
-    from: 'sender@example.org',
-    subject: 'Hello world',
-    text: 'Hello plain world!',
-    html: '<p>Hello HTML world!</p>',
-    attachments: [
-        {
-            content: data.toString('base64'),
-            filename: filename,
-            type: fileType,
-            disposition: 'attachment',
-        },
-    ],
+  to: "cesarquintini@gmail.com",
+  from: "noreply@cocomarch.com",
+  subject: "Deployment" + process.env.SUCCESS ? "Successful" : "Failed",
+  text:
+    process.env.REPOSITORY + " " + process.env.REF + " " + process.env.EVENT + process.env.SUCCESS
+      ? " Successful"
+      : " Failed",
+  html:
+    "<p>" + process.env.REPOSITORY + " " + process.env.REF + " " + process.env.EVENT + process.env.SUCCESS
+      ? " Successful"
+      : " Failed" + "</p>",
 };
 
 sgMail
-    .send(msg)
-    .then(() => console.log('Mail sent successfully'))
-    .catch(error => console.error(error.toString()));
+  .send(msg)
+  .then(() => console.log("Mail sent successfully"))
+  .catch((error) => console.error(error.toString()));

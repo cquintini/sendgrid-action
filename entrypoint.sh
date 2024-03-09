1409f9fc2a1ca965af01aa4b5ae1bc447ffeaded
Cesar Quintini <cesarquintini@gmail.com>
2024-03-09 16:55:17 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -l
 set -euo pipefail
 
-echo "$ls"
-
-echo "$ls ${GITHUB_WORKSPACE}/.github/"
+chmod +x "./.github/sendgrid.js"
 
 # Set the default path to the script
-SCRIPT_FILEPATH="${GITHUB_WORKSPACE}/.github/sendgrid.js"
+SCRIPT_FILEPATH="./.github/sendgrid.js"
 
 # Execute the script
 $SCRIPT_FILEPATH
